90ba3d2dfef5e2e358e04f4d4a883e0437001a38
Дом <APCH76@mail.ru>
2022-11-19 18:19:42 +0300

    Init

---
+++ app/src/main/java/Main.kt
@@ -0,0 +1,11 @@
+ fun main() {
+
+     var predators = listOf<String>("Плотоядные:","Волк, " ,"Тигр")
+     for (predator in predators)
+         println(predator)
+
+
+      var herbivoreses = listOf<String>("Травоядные:","Корова, " ,"Лошадь")
+      for (herbivores in herbivoreses)
+           print(herbivores)
+}
